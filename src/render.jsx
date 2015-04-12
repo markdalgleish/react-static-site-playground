aebd5947ec51276441444eb257e2b4da7cf6c518
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2015-04-12 17:08:20 +1000

    Use node callback for render instead of promises

--- src/render.jsx
+++ src/render.jsx
@@ -1,17 +1,14 @@
 var React = require('react');
 var Router = require('react-router');
-var Promise = require('bluebird');
 
 var Routes = require('./routes/routes.jsx');
 var template = require('./template.ejs');
 
-module.exports = function(data) {
-  return new Promise(function(resolve, reject) {
-    Router.run(Routes, data.path, function(Handler) {
-      resolve(template({
-        html: React.renderToString(<Handler />),
-        assets: data.assets
-      }));
-    });
+module.exports = function(data, callback) {
+  Router.run(Routes, data.path, function(Handler) {
+    callback(null, template({
+      html: React.renderToString(<Handler />),
+      assets: data.assets
+    }));
   });
 };
