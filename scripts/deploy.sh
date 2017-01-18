2d840606b42d1de4da3d7f295a851719adeb4c4e
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2017-01-19 09:57:14 +1100

    Configure deployment git user globally

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git config user.name "Travis-CI"
-git config user.email "travis@example.com"
+git config --global user.name "Travis-CI"
+git config --global user.email "travis@example.com"
 
 npm run deploy -- -r https://${GH_TOKEN}@github.com/markdalgleish/react-static-site-playground
