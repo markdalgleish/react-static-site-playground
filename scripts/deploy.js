498a378b5b038ea695e0c62f956186d0f9271d27
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2017-01-19 22:45:40 +1100

    Filter GH_TOKEN environment variable from deployment logs

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -10,6 +10,10 @@ var repoUrl = process.env.GH_TOKEN ?
 ghpages.publish(path.join(__dirname, '../dist'), {
   repo: repoUrl,
   logger: function(message) {
+    if (process.env.GH_TOKEN) {
+      message = message.replace(new RegExp(process.env.GH_TOKEN, 'g'), '[GH_TOKEN]');
+    }
+
     console.log(message);
   }
 }, function() {
